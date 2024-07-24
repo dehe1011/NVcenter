0cf30f5c3db39f24990a2a14f2498c7eb43c8288
Dennis Herb <dennis.herb>
2024-07-24 14:44:56 +0200

    new commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ExampleProject_11",
-    version="0.1.0",
+    version="0.1.1",
     author="Dennis Herb",
     author_email="dennis.herb@uni-ulm.de",
     description="An example project for testing.",
