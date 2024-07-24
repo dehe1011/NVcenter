d369b2dae3cdd92c36d7fb111616a189a6a05f2e
Dennis Herb <dennis.herb>
2024-07-24 13:06:26 +0200

    new commit

--- tests/test_my_add.py
+++ tests/test_my_add.py
@@ -1,6 +1,6 @@
 import pytest
 from src import my_add
 
-@pytest.mark.parametrize("x, y, expected", [(1,2,3), (4,-2,2)] )
-def test_my_add(x, y, expected):
-    assert my_add(x, y) == expected
+# @pytest.mark.parametrize("x, y, expected", [(1,2,3), (4,-2,2)] )
+# def test_my_add(x, y, expected):
+#     assert my_add(x, y) == expected
